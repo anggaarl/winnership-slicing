d298f0d064cbeee6b06fd52b94b492d41074a805
Angga <anggaairulr@gmail.com>
2024-09-30 22:23:01 +0700

    first commit

---
+++ resources/views/landing_page.blade.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords" content="Bootstrap, Landing page, Template, Business, Service">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="author" content="Grayrids">
+    <title>Winnership - Angga Airul Rohman</title>
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="img/wesclic.png" type="image/png">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/LineIcons.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/nivo-lightbox.css">
+    <link rel="stylesheet" href="css/main.css">    
+    <link rel="stylesheet" href="css/responsive.css">
+
+  </head>
+  
+  <body>
+
+    <!-- Header Section Start -->
+    <header id="home" class="hero-area">    
+      <div class="overlay">
+        <span></span>
+        <span></span>
+      </div>
+      <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="" class="navbar-brand"><img src="img/logo-umy.png" alt="" width="220" height="40"></a>       
+                <a href="" class="navbar-brand"><img src="img/cdc.png" alt="" width="190" height="50"></a>       
+            </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="lni-menu"></i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav w-100 justify-content-center">
+              <li class="nav-item">
+                <a class="nav-link page-scroll" href="#home">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link page-scroll" href="#services">About</a>
+              </li>  
+              <li class="nav-item">
+                <a class="nav-link page-scroll" href="https://linktr.ee/cdc_umy">Contact</a>
+              </li> 
+              </ul>
+              
+              <ul>
+              <li class="nav-item ml-auto">
+                <a class="btn btn-singin" href="#">Sign Up</a>
+              </li>
+              </ul>
+          </div>
+        </div>
+      </nav>  
+      <div class="container">      
+        <div class="row space-100">
+          <div class="col-lg-6 col-md-12 col-xs-12">
+            <div class="contents">
+              <h2 class="head-title">Empowering Alumni With The University Alumni Tracer</h2>
+              <p>Track the journey of our alumni as they navigate their professional paths, fostering connections and opportunities within our vibrant community.</p>
+              <div class="header-button">
+                <a href="https://linktr.ee/cdc_umy" class="btn btn-border-filled page-scroll">Contact Us</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-12 col-xs-12 p-0">
+            <div class="intro-img">
+              <img src="img/hero.png" alt="">
+            </div>            
+          </div>
+        </div> 
+      </div>             
+    </header>
+    <!-- Header Section End --> 
+
+
+    <!-- Services Section Start -->
+    <section id="services" class="section">
+      <div class="container">
+        <div class="about">
+            <div class="title">
+
+                <h2>Apa yang kamu dapatkan di Tracer Study?</h2>
+            </div>
+        </div>
+
+        <div class="row">
+          <!-- Start Col -->
+          <div class="col-lg-6 col-md-6 col-xs-12">
+            <div class="services-item text-center">
+              <div class="icon">
+                <img src="img/data.png"></img>
+              </div>
+              <h4>Data Kuesioner</h4>
+              <p>Manage your profile after graduate from university</p>
+            </div>
+          </div>
+          <!-- End Col -->
+          <!-- Start Col -->
+          <div class="col-lg-6 col-md-6 col-xs-12">
+            <div class="services-item text-center">
+              <div class="icon">
+                <img src="img/cv.png"></img>
+              </div>
+              <h4>CV Builder</h4>
+              <p>Create a professional CV that highlights your skills</p>
+            </div>
+          </div>
+          <!-- End Col -->
+
+        </div>
+      </div>
+    </section>
+    <!-- Services Section End -->
+
+
+
+    <!-- Business Plan Section Start -->
+    <section id="business-plan">
+      <div class="container">
+
+        <div class="row align-items-center">
+            <!-- Start Col -->
+            <div class="col-lg-7 col-md-12 pl-4">
+                <div class="business-item-info">
+                    <h3>Career Development UMY</h3>
+                    <p>Career Development UMY is dedicated to empowering students and alumni in navigating their professional journeys. Through a range of services—including career counseling, job placement assistance, and skill development workshops—we aim to bridge the gap between academic experiences and industry expectations, ensuring that our graduates are well-prepared to excel in their chosen fields. <br> <br>
+                    By fostering partnerships with various industries, Career Development UMY provides access to diverse internship and employment opportunities. We also offer resources such as resume building tools and interview preparation sessions to enhance job readiness. Our commitment is to support you in achieving your career goals and contributing meaningfully to the workforce.</p>
+
+                </div>
+            </div>
+            <!-- End Col -->
+            
+            <!-- Start Col -->
+            <div class="col-lg-5 col-md-12 pl-0 pt-0 pr-5">
+              <div class="business-item-img">
+                <img src="img/business/business-img.png" class="img-fluid" alt="">
+              </div>
+            </div>
+            <!-- End Col -->
+        </div>
+      </div>
+    </section>
+    <!-- Business Plan Section End -->
+
+
+    <!-- Recent Showcase Section Start -->
+    <section id="showcase">
+      <div class="container-fluid right-position">
+        <!-- Start Row -->
+        <div class="row gradient-bg">
+          <div class="col-lg-12">
+            <div class="showcase-text section-header text-center">  
+              <div>   
+                <h2 class="section-title">Recent Achievement</h2>
+                <div class="desc-text">
+                  <p>Layanan CDC UMY meliputi penyebaran informasi lowongan pekerjaan dari perusahaan, rekrutmen di kampus, webinar peningkatan soft skills dan hard skills, career coaching, penelusuran alumni, survei penggunana lulusan, dan FGD pengguna lulusan</p>  
+                </div>
+              </div> 
+            </div>
+          </div>
+
+        </div>
+        <!-- End Row -->
+        <!-- Start Row -->
+        <div class="row justify-content-center showcase-area">
+          <div class="col-lg-12 col-md-12 col-xs-12 pr-0">
+            <div class="showcase-slider owl-carousel">
+              <div class="item">
+                <div class="screenshot-thumb">
+                  <img src="img/showcase/01.png" class="img-fluid" alt="" />
+                  <div class="hover-content text-center">
+                  </div>
+
+                </div>
+              </div>
+              <div class="item">
+                <div class="screenshot-thumb">
+                  <img src="img/showcase/02.png" class="img-fluid" alt="" />
+                  <div class="hover-content text-center">
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="screenshot-thumb">
+                  <img src="img/showcase/03.png" class="img-fluid" alt="" />
+                  <div class="hover-content text-center">
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="screenshot-thumb">
+                  <img src="img/showcase/06.png" class="img-fluid" alt="" />
+                  <div class="hover-content text-center">
+                  </div>
+                </div>
+              </div>
+              <div class="item">
+                <div class="screenshot-thumb">
+                  <img src="img/showcase/05.png" class="img-fluid" alt="" />
+                  <div class="hover-content text-center">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- End Row -->
+      </div>
+    </section>
+    <!-- Recent Showcase Section End --> 
+
+    <!-- Go To Top Link -->
+    <a href="#" class="back-to-top">
+      <i class="lni-chevron-up"></i>
+    </a> 
+
+    <!-- Preloader -->
+    <div id="preloader">
+      <div class="loader" id="loader-1"></div>
+    </div>
+    <!-- End Preloader -->
+
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="js/jquery-min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.js"></script>      
+    <script src="js/jquery.nav.js"></script>    
+    <script src="js/scrolling-nav.js"></script>    
+    <script src="js/jquery.easing.min.js"></script>     
+    <script src="js/nivo-lightbox.js"></script>     
+    <script src="js/jquery.magnific-popup.min.js"></script>       
+    <script src="js/main.js"></script>
+    
+  </body>
+</html>
